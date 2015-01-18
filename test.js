1f06fd2bcf0d8f36f96f13b591353fedb34be428
vesselin <vesselin@vesselin-IdeaPad-U430-Touch>
2015-01-18 23:02:22 +0100

    collistion implemented and lib integrated
    Change-Id: I3254d7fb432a6e9d8e82c803c021167f69dcce1d

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function start() {
 
 	Crafty.init(500, 350, document.getElementById('game'));
 
-	// when it walks 
+	// when it walks
 	Crafty.e('Floor, 2D, Canvas, Color').attr({
 		x : 0,
 		y : 250,
@@ -11,49 +11,41 @@ function start() {
 		h : 10
 	}).color('green');
 
+	Crafty
+			.e("2D, Canvas, Color, Fourway, Gravity, Collision")
+			.attr({
+				x : 20,
+				y : 20,
+				w : 30,
+				h : 30
+			})
+			.color('#11AAB0')
+			.fourway(4)
+			.gravity('Floor')
+			.checkHits('Solid')
+			// check for collisions with entities that have the Solid component
+			// in each frame
+			.bind(
+					'HitOn',
+					function(hitData) {
 
-Crafty.e("2D, Canvas, Color, Fourway, Gravity, Collision").attr({
-		x : 30,
-		y : 20,
-		w : 20,
-		h : 30
-	}).color('#F11AAB0').fourway(4).gravity('Floor')
-    .checkHits('Solid') // check for collisions with entities that have the Solid component in each frame
-    .bind('HitOn', function(hitData) {
+						console
+								.log('Collision with Solid entity occurred for the first time.');
+						console.log("x=" + this.x);
 
-        console.log('Collision with Solid entity occurred for the first time.');
-
-		if (this._movement) {
-			this.x -= this._movement.x;
-			this.y -= this._movement.y; 
-			this._movement.x=0;
-			console.log('stop the movement');	
-		}
-
-		//return this;
-    })
-    .bind('HitOff', function(comp) {
-        console.log('Collision with Solid entity ended.');
-    });
-
-
-	// player
-//	Crafty.e('2D, Canvas, Color, Fourway, Gravity').attr({
-//		x : 0,
-//		y : 0,
-//		w : 10,
-//		h : 10
-//	}).color('#F00').fourway(4).gravity('Floor').checkHits('Solid') 
-//    .bind("HitOn", function(hitData) {
-//        console.log("Collision with Solid entity occurred for the first time.");
-//    })
-//    .bind("HitOff", function(comp) {
-//        console.log("Collision with Solid entity ended.");
-//    });
-
+						if (this._movement) {
+							this.x -= this._movement.x;
+							this.y -= this._movement.y;
+							console.log('stop the movement');
+						}
+						this.resetHitChecks();
+						// return this;
+					}).bind('HitOff', function(comp) {
+				console.log('Collision with Solid entity ended.');
+			});
 
 	// wall on the right
-	Crafty.e('Wall, 2D, Canvas, Color, Solid').attr({
+	Crafty.e('Wall, 2D, Canvas, Color, Solid, Floor').attr({
 		x : 0,
 		y : 230,
 		w : 10,
@@ -61,16 +53,11 @@ Crafty.e("2D, Canvas, Color, Fourway, Gravity, Collision").attr({
 	}).color('green');
 
 	// wall on the left
-	Crafty.e('Wall, 2D, Canvas, Color, Solid').attr({
-		x : 470,
+	Crafty.e('Wall, 2D, Canvas, Color, Solid, Floor').attr({
+		x : 370,
 		y : 230,
 		w : 10,
 		h : 20
 	}).color('green');
 
 }
-
-
-
-
-
